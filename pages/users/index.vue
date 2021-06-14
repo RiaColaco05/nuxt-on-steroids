0b114bf7038ef7e8a9d0dd98257bab652ade327a
RiaColaco05 <32483762+RiaColaco05@users.noreply.github.com>
2021-06-14 18:07:12 +0530

    Initial Commit
    Learning nuxt  on steroid chap 2

---
+++ pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <h1>The users view </h1>
+    <input type="text" v-model="userId" />
+    <button @click="onLoadUser">Load User</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            userId: null
+        }
+    },
+    methods: {
+        onLoadUser(){
+            this.$router.push({path : '/users/' + this.userId})
+        }
+    }
+}
+</script>
