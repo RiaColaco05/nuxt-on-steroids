c9953a270a4226a092d75ce859e7d6dc96f57c4c
RiaColaco05 <32483762+RiaColaco05@users.noreply.github.com>
2021-07-06 18:40:24 +0530

    Pages, Routing & Views: 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'image/x-icon', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap' }
+
+
     ]
   },
 
---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "About Me" Page!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas pariatur minima nulla laudantium eum repellendus, id sint animi sit minus delectus, quasi reprehenderit voluptatem eaque quisquam, distinctio consectetur nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas pariatur minima nulla laudantium eum repellendus, id sint animi sit minus delectus, quasi reprehenderit voluptatem eaque quisquam, distinctio consectetur nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas pariatur minima nulla laudantium eum repellendus, id sint animi sit minus delectus, quasi reprehenderit voluptatem eaque quisquam, distinctio consectetur nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas pariatur minima nulla laudantium eum repellendus, id sint animi sit minus delectus, quasi reprehenderit voluptatem eaque quisquam, distinctio consectetur nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas pariatur minima nulla laudantium eum repellendus, id sint animi sit minus delectus, quasi reprehenderit voluptatem eaque quisquam, distinctio consectetur nobis.</p>
+    </div>
+</template>
+
+<style scoped>
+.about-page{
+    text-align: center;
+    padding: 30px;
+}
+</style>
