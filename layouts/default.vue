8c0922fac60936f93e20203f58bd99f758588a48
RiaColaco05 <32483762+RiaColaco05@users.noreply.github.com>
2021-09-21 15:53:21 +0530

    ch 3, lesn 6: header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <Header />
+    <!-- <Header /> -->
+    <TheHeader @sidenavToggle='displaySidenav = !displaySidenav' /> 
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" /> 
     <Nuxt />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
 export default {
     components :{
-      Header
+      Header,
+      TheHeader,
+      TheSidenav
+    },
+    data(){
+      return {
+        displaySidenav : false
+      }
     }
     
 }
