c67eb35ea5e1e46ca0c8ac93c77fd208a098e70e
RiaColaco05 <32483762+RiaColaco05@users.noreply.github.com>
2021-09-21 17:51:18 +0530

    ch 3, lesn 9: header

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+            <section class="featured-posts">
+ <PostPreview id="1" previewImg="https://assets.thehansindia.com/h-upload/2021/07/31/1600x960_1092805-tech.jpg" title="hello there first!" previewText="This is my fisrt post!"/>
+ <PostPreview id="2" previewImg="https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg" title="hello there second!" previewText="This is my second post!"/>
+ <PostPreview id="3" previewImg="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg" title="hello there third!" previewText="This is my third post!"/>
+</section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components:{
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,7 @@
 <section class="intro">
   <h1>Get the latest tech news!</h1>
 </section>
-<section class="featured-posts">
- <PostPreview id="1" previewImg="https://assets.thehansindia.com/h-upload/2021/07/31/1600x960_1092805-tech.jpg" title="hello there first!" previewText="This is my fisrt post!"/>
- <PostPreview id="2" previewImg="https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg" title="hello there second!" previewText="This is my second post!"/>
- <PostPreview id="3" previewImg="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg" title="hello there third!" previewText="This is my third post!"/>
-</section>
+ <PostList />
 
     <!-- <div>
       <Logo />
@@ -38,10 +34,10 @@
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 export default {
   components:{
-    PostPreview
+    PostList
   }
 }
 </script>
